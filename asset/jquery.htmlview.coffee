f3c317c9c2fc1827f3fe4526e1e2e7d57ba0aa80
Yokota Kenji <mail@yokotakenji.me>
2014-08-15 04:02:54 +0900

    タブの前に不要な空白がある場合削除するように変更

--- asset/jquery.htmlview.coffee
+++ asset/jquery.htmlview.coffee
@@ -1,23 +1,33 @@
 (($)->
 	$.fn.htmlview = (ops)->
 		# ----------------------------------------------------------------------
+		config = $.extend({
+			'posFix' : true
+		},ops)
+		# ----------------------------------------------------------------------
 		_escape = (str)->
-			if(str && typeof str == 'string')
-				str = str.replace(/\&lt;/g,'<')
-				str = str.replace(/\&gt;/g,'>')
-				return str.replace(/[&<>"']/g,($0)->
-					return {
-						'\&' : '&#38;'
-						'\<' : '&#60;'
-						'\>' : '&#62;'
-						'\"' : '&#34;'
-						'\'' : '&#39;'
-					}[$0]
+			str = str.replace(/^[\s\S]/,'')
+			str = str.replace(/[\s]$/,'')
+			# str = str.replace(/\&lt;/g,'<')
+			# str = str.replace(/\&gt;/g,'>')
+			# 
+			if(config.posFix)
+				trimSpace = null
+				str       = str.replace(/^([\s]*)(?=\S)/,($0)->
+					trimSpace = $0;
+					return $0;
 				)
-			return
+				strArr = str.split(/\r\n|\r|\n/)
+				l      = strArr.length
+				str    = ''
+				for line,i in strArr
+					line = line.replace(trimSpace,'')
+					str += if(i != l-1) then line + '\n' else line
+			return str
 		# ----------------------------------------------------------------------
 		return this.each(()->
-			$(this).html(_escape($(this).html()))
-			return
+			str = $(this).html()
+			if(str && typeof str == 'string') then $(this).text(_escape(str))
+			return this
 		)
 )(jQuery)
--- build/jquery.htmlview.js
+++ build/jquery.htmlview.js
@@ -1,23 +1,37 @@
 (function($) {
   return $.fn.htmlview = function(ops) {
-    var _escape;
+    var config, _escape;
+    config = $.extend({
+      'posFix': true
+    }, ops);
     _escape = function(str) {
-      if (str && typeof str === 'string') {
-        str = str.replace(/\&lt;/g, '<');
-        str = str.replace(/\&gt;/g, '>');
-        return str.replace(/[&<>"']/g, function($0) {
-          return {
-            '\&': '&#38;',
-            '\<': '&#60;',
-            '\>': '&#62;',
-            '\"': '&#34;',
-            '\'': '&#39;'
-          }[$0];
+      var i, l, line, strArr, trimSpace, _i, _len;
+      str = str.replace(/^[\s\S]/, '');
+      str = str.replace(/[\s]$/, '');
+      if (config.posFix) {
+        trimSpace = null;
+        str = str.replace(/^([\s]*)(?=\S)/, function($0) {
+          trimSpace = $0;
+          return $0;
         });
+        strArr = str.split(/\r\n|\r|\n/);
+        l = strArr.length;
+        str = '';
+        for (i = _i = 0, _len = strArr.length; _i < _len; i = ++_i) {
+          line = strArr[i];
+          line = line.replace(trimSpace, '');
+          str += i !== l - 1 ? line + '\n' : line;
+        }
       }
+      return str;
     };
     return this.each(function() {
-      $(this).html(_escape($(this).html()));
+      var str;
+      str = $(this).html();
+      if (str && typeof str === 'string') {
+        $(this).text(_escape(str));
+      }
+      return this;
     });
   };
 })(jQuery);
